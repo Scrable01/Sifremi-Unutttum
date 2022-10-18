9e01548b7d5e3f252e422fe9009a96b1f37d71e5
Enes Memduhoglu <eneshan034@gmail.com>
2022-10-18 17:24:34 +0300

    Sifreyi unutunca mil gönderme

---
+++ WinFormOturumAcmaIslemleri/WinFormOturumAcmaIslemleri/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WinFormOturumAcmaIslemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MailKontrol = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SifreKontrol = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MailKontrol
+            // 
+            this.MailKontrol.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MailKontrol.Location = new System.Drawing.Point(240, 105);
+            this.MailKontrol.Multiline = true;
+            this.MailKontrol.Name = "MailKontrol";
+            this.MailKontrol.Size = new System.Drawing.Size(282, 21);
+            this.MailKontrol.TabIndex = 0;
+            this.MailKontrol.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(101, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Mail Adresiniz:";
+            // 
+            // SifreKontrol
+            // 
+            this.SifreKontrol.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.SifreKontrol.Location = new System.Drawing.Point(240, 132);
+            this.SifreKontrol.Multiline = true;
+            this.SifreKontrol.Name = "SifreKontrol";
+            this.SifreKontrol.PasswordChar = '*';
+            this.SifreKontrol.Size = new System.Drawing.Size(282, 21);
+            this.SifreKontrol.TabIndex = 5;
+            this.SifreKontrol.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(101, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Şifreniz:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(244, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 53);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(244, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 42);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Şifremi unuttum";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(622, 374);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SifreKontrol);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MailKontrol);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox MailKontrol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SifreKontrol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinFormOturumAcmaIslemleri/WinFormOturumAcmaIslemleri/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinFormOturumAcmaIslemleri.Models;
+
+namespace WinFormOturumAcmaIslemleri
+{
+    public partial class Form1 : Form
+    {
+        WinFormOturumAcmaIslemleriEntitiesConnectionDb db = new WinFormOturumAcmaIslemleriEntitiesConnectionDb();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        public static int Id;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            var Durum = db.PersonelGirisTablosu.FirstOrDefault(x => x.MailAdress == MailKontrol.Text && x.Password == SifreKontrol.Text);
+            if (Durum != null)
+            {
+                Id = Durum.Id;
+                GirisBasarili gb = new GirisBasarili();
+                gb.ShowDialog();
+
+            }
+            else
+            {
+                MessageBox.Show("Girilen bilgiler hatalıdır", "Durum", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ŞİfreSıfırlama st= new ŞİfreSıfırlama();
+            st.ShowDialog();
+        }
+    }
+}
